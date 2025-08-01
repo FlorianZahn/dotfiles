f06f8138ae8798a50565e77866a6247f8b915e06
Florian Zahn <anderes.zeug@outlook.com>
2025-08-01 03:41:02 +0200

    a

--- nixConfig/modules/users/florian/default.nix
+++ nixConfig/modules/users/florian/default.nix
@@ -1,21 +1,3 @@
 { pkgs, ... }: {
-    home.username = "florian";
-    home.homeDirectory = "/home/florian";
 
-    home.packages = with pkgs; [
-        firefox
-        kitty
-        btop
-    ];
-
-    programs.zsh = {
-        enable = true;
-        oh-my-zsh = {
-        enable = true;
-        theme = "agnoster";
-        plugins = [ "git" ];
-        };
-    };
-
-    home.stateVersion = "24.05";
 }
